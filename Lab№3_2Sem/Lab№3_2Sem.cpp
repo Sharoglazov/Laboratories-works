9cb360db63c3f95dc9d074a1d52040127865407c
Sharoglazov <148006285+Sharoglazov@users.noreply.github.com>
2024-02-05 19:35:35 +0500

    Add files via upload

---
+++ Lab№3_2Sem/Lab№3_2Sem.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+const int n = 10;
+int a[n];
+int temp, k;
+
+int main() {
+	srand(time(NULL));
+	setlocale(LC_ALL, "RU");
+	cout << "Введите k: ";
+	cin >> k;
+
+	for (int i = 0; i < n; i++) {
+		a[i] = rand() % 50;
+		cout << a[i] << " ";
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < k; i++) {
+		temp = a[0];
+		for (int j = 0; j < n-1; j++) {
+			a[j] = a[j+1];
+		}
+		a[n-1] = temp;
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+
+	return 0;
+}
